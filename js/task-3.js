f272ba30b1f8ef3817048fcb3ff52823f4f998dd
XviacheslavX <viacheslav_khylchuk@ukr.net>
2025-03-23 10:43:40 +0100

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = Number.parseFloat(content);
-    let paddingtWidth = Number.parseFloat(padding) * 2;
-    let borderWidth = Number.parseFloat(border) * 2;
-    const totalWidth = contentWidth + paddingtWidth + borderWidth;
-    return `${totalWidth}`;
+    content = Number.parseInt(content);
+    padding = Number.parseInt(padding) * 2;
+    border = Number.parseInt(border) * 2;
+    return content + border + padding;
 }
-
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
